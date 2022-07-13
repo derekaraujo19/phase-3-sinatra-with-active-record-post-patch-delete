7b7ecf9f1e17e219c23cfa40fe44825fcff83caa
Derek Araujo <derekaraujo19@gmail.com>
2022-07-13 21:37:33 +0200

    lab complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,47 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  get '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.to_json
+  end
+
+  get '/reviews' do
+    reviews_all = Review.all
+    reviews_all.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+
+  # Access the data in the body of the request
+  # Use that data to update the review in the database
+  # Send a response with updated review as JSON
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
 end
